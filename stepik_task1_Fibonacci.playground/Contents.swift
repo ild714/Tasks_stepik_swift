5cebb735d384c4cb40413013dc0ad5f902aa2864
Ildar <ildarnigmetzjanov@gmail.com>
2019-07-04 16:36:25 +0300

    first task

--- stepik_task1_Fibonacci.playground/Contents.swift
+++ stepik_task1_Fibonacci.playground/Contents.swift
@@ -1,20 +1,23 @@
 import UIKit
 
-enum FibonachiError : Error{
-    case argumentShouldNotBeNegative
+enum Errors : Error {
+    case errorInNumber
 }
 
-func fibonachi (n: Int) throws -> Int {
-    guard n>=0 else {throw FibonachiError.argumentShouldNotBeNegative}
-
-    var first = 0
-    var next = 1
-
-    for _ in 0..<n {
-        next = first + next
-        first = next - first
-}
-return first
+func fibonachi(n: Int) throws -> Int {
+    guard n >= 0 else {
+        throw Errors.errorInNumber
+    }
+    
+    var a1 = 1
+    var a2 = 1
+    var a_n = 0
+    for _ in 2..<n {
+        a_n = a1 + a2
+        a1 = a2
+        a2 = a_n
+    }
+    return a_n
 }
 
 try fibonachi(n: -1)
