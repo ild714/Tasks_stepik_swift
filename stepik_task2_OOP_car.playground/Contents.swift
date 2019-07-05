11861b5218c199150735c7812b1d1c56349c0bcf
Ildar <ildarnigmetzjanov@gmail.com>
2019-07-05 09:57:37 +0300

    edit task2

--- stepik_task2_OOP_car.playground/Contents.swift
+++ stepik_task2_OOP_car.playground/Contents.swift
@@ -1,74 +1,81 @@
 import UIKit
 
-enum GoError: Error {
+enum GoError : Error {
     case changeDistance
 }
 
 struct Go {
-    enum Direction{
-        case forward
-        case back
-        case left
+    enum Direction {
         case right
+        case left
+        case down
+        case up
     }
-    let direction: Direction
-    let distance: Double
     
-    init(direction:Direction,distance:Double) throws {
-        guard distance > 0 else {
+    let whereGo : Direction
+    let howLong : Double
+    
+    init(whereGo : Direction , howLong : Double) throws {
+        guard howLong > 0 else {
             throw GoError.changeDistance
         }
-        self.direction=direction
-        self.distance=distance
+        self.whereGo = whereGo
+        self.howLong = howLong
     }
 }
 
 class Car {
-    private let mark: String
-    private let type: String
     
-    init(mark:String, type: String ){
-        self.mark = mark
-        self.type = type
+    static let wheelAmount = 4
+    class var sellAmount : Int {
+        return 5
     }
     
-    // forward
-    func forward(distance: Double){
-        print ("Duration forward = \(distance)")
+    let model : String
+    let color : UIColor
+    var recordSpeed : Int?
+    
+    init(model: String,color: UIColor, recordSpeed : Int?){
+        self.model = model
+        self.color = color
+        self.recordSpeed = recordSpeed
     }
     
-    // back
-    func back(distance: Double){
-        print ("Duration back = \(distance)")
+    func right (dist : Double) {
+        print (dist)
     }
     
-    // left
-    func left(distance: Double){
-        print ("Duration left = \(distance)")
+    func left (dist : Double) {
+        print (dist)
     }
     
-    // right
-    func right(distance: Double){
-        print ("Duration right = \(distance)")
+    func down (dist : Double) {
+        print (dist)
+    }
+    
+    func up (dist : Double) {
+        print (dist)
     }
     
     
-    func execute(go:Go){
-        switch go.direction{
-        case .forward:
-            forward(distance: go.distance)
-        case .back:
-            back(distance: go.distance)
-        case .left:
-            left(distance : go.distance)
-        case .right:
-            right(distance: go.distance)
-        }
+    func execute (_ strc : Go) {
+        switch strc.whereGo
+        {
+        case .right :
+            right(dist : strc.howLong)
+        case .left :
+             left(dist : strc.howLong)
+        case .down :
+             up(dist : strc.howLong)
+        default :
+             down(dist : strc.howLong)
     }
-    
 }
 
-let car1: Car = Car(mark: "bmv", type: "1")
-let go: Go = try Go(direction: .left, distance: 100)
+}
 
-car1.execute(go:go)
+let wh = try Go(whereGo: .right, howLong: 50)
+
+let car1 = Car(model: "tt", color: UIColor.black, recordSpeed: 300)
+
+car1.execute(wh)
