69fd8ccec4e1c781d6e1ca7922d84ac63d6da004
Ildar <42585624+ildar777@users.noreply.github.com>
2019-07-07 23:20:20 +0300

    edit

---
+++ Notes/Notes/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Notes
+
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let serverUrl = Bundle.main.object(forInfoDictionaryKey: "serverUrl") as? String
+        print(serverUrl ?? "")
+        
+        /*
+        var n = 100
+        
+        while true {
+            print (n)
+        }
+        */
+        return true
+    }
+}
+
